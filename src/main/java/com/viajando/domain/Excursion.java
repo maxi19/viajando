770aff5d0d1ecbe4e3f03df12aeda99b38236046
bririnaa <brisamartino656@gmail.com>
2025-06-16 09:41:59 -0300

    Se agrega clase excursion

---
+++ src/main/java/com/viajando/domain/Excursion.java
@@ -0,0 +1,95 @@
+package com.viajando.domain;
+
+import java.time.LocalDate;
+
+public class Excursion {
+
+	private int id;
+	private String nombre;
+	private String descripcion;
+	private LocalDate fecha_inicio;
+	private LocalDate fecha_fin;
+	private int precio;
+	private String destino;
+	private double estrellas;
+	
+	public Excursion(int id, String nombre, String descripcion, LocalDate fecha_inicio, LocalDate fecha_fin, int precio,
+			String destino, double estrellas) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.fecha_inicio = fecha_inicio;
+		this.fecha_fin = fecha_fin;
+		this.precio = precio;
+		this.destino = destino;
+		this.estrellas = estrellas;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public LocalDate getFecha_inicio() {
+		return fecha_inicio;
+	}
+
+	public void setFecha_inicio(LocalDate fecha_inicio) {
+		this.fecha_inicio = fecha_inicio;
+	}
+
+	public LocalDate getFecha_fin() {
+		return fecha_fin;
+	}
+
+	public void setFecha_fin(LocalDate fecha_fin) {
+		this.fecha_fin = fecha_fin;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	public String getDestino() {
+		return destino;
+	}
+
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+
+	public double getEstrellas() {
+		return estrellas;
+	}
+
+	public void setEstrellas(double estrellas) {
+		this.estrellas = estrellas;
+	}
+	
+	
+	
+}
