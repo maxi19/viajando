d4842dbf7777d9f45f792021e1687a8647d8a6ae
TatianaMartino <tatianamartino90@gmail.com>
2025-06-19 18:39:19 -0300

    subo service

---
+++ src/main/java/com/viajando/service/vuelo/VueloService.java
@@ -0,0 +1,18 @@
+package com.viajando.service.vuelo;
+
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.List;
+
+import com.viajando.domain.Vuelo;
+
+public interface VueloService {
+	
+public void crearVuelo(String destino, String ida, String vuelta, int precio, int estrellas, LocalTime hora_ida, LocalTime hora_vuelta) throws Exception;
+	
+	public List<Vuelo> listarVuelos() throws Exception;
+	
+	
+
+}
---
+++ src/main/java/com/viajando/service/vuelo/VueloServiceImp.java
@@ -0,0 +1,28 @@
+package com.viajando.service.vuelo;
+
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.List;
+
+import com.viajando.dao.vuelo.VueloDaoImp;
+import com.viajando.dao.vuelo.VueloDao;
+import com.viajando.domain.Vuelo;
+
+public class VueloServiceImp implements VueloService{
+	
+	private VueloDao vueloDao = new VueloDaoImp();
+
+	@Override
+	public void crearVuelo(String destino, String ida, String vuelta, int precio, int estrellas, LocalTime hora_ida, LocalTime hora_vuelta) throws Exception {
+		vueloDao.crearVuelo(destino, ida, vuelta, precio, estrellas, hora_vuelta, hora_vuelta);
+		
+	}
+
+	@Override
+	public List<Vuelo> listarVuelos() throws Exception {
+		return vueloDao.listarVuelos();
+	}
+
+	
+}
