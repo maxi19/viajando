9c40c402300a5bafd76851606f5bd5d2cde49ad3
Bririnaa <brisamartino656@gmail.com>
2025-06-22 20:48:39 -0300

    Se modifica controller Excursion

--- src/main/java/com/viajando/controller/excursion/ExcursionController.java
+++ src/main/java/com/viajando/controller/excursion/ExcursionController.java
@@ -3,6 +3,7 @@ package com.viajando.controller.excursion;
 import java.io.IOException;
 
 import java.io.PrintWriter;
+import java.time.LocalDate;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,8 +12,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.viajando.service.excursion.ExcursionService;
 import com.viajando.service.excursion.ExcursionServiceImp;
+import com.viajando.parser.parser;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 @WebServlet("/excursionController")
 public class ExcursionController extends HttpServlet {
@@ -24,15 +27,20 @@ public class ExcursionController extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		try {
-			Gson json = new Gson();
+			
+			Gson gson = new GsonBuilder()
+			        .registerTypeAdapter(LocalDate.class, new parser())
+			        .create();
+			
 			PrintWriter out = resp.getWriter();
 			resp.setContentType("application/json");
 			resp.setCharacterEncoding("utf-8");
-			out.print(json.toJson(excursionService.list()));
+			out.print(gson.toJson(excursionService.list()));
 			out.flush();
 
 		} catch (Exception e) {
-
+			
+System.out.println(e.getMessage());
 		}
 
 	}
