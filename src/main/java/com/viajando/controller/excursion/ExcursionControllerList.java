b032b9139e6fc8cf4d1f3b8181ebb7b4d10a22b0
Bririnaa <brisamartino656@gmail.com>
2025-06-27 00:18:44 -0300

    Controller Excursion

--- src/main/java/com/viajando/controller/excursion/ExcursionControllerList.java
+++ src/main/java/com/viajando/controller/excursion/ExcursionControllerList.java
@@ -1,6 +1,7 @@
 package com.viajando.controller.excursion;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.viajando.service.excursion.ExcursionServiceImp;
 import com.google.gson.Gson;
+import com.viajando.dao.DestinoDao;
+import com.viajando.domain.Destino;
 import com.viajando.service.excursion.ExcursionService;
 
 
@@ -19,12 +22,15 @@ public class ExcursionControllerList extends HttpServlet {
        
     private ExcursionService excursionService = new ExcursionServiceImp();
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		DestinoDao dao = new DestinoDao();
+
 		try {
+			List<Destino> destinos = dao.list();
 			Gson json = new Gson();
 			PrintWriter out = resp.getWriter();
 			resp.setContentType("application/json");
 			resp.setCharacterEncoding("utf-8");
-			out.print(json.toJson(excursionService.list()).toString());
+			out.print(json.toJson(destinos));
 			out.flush();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
