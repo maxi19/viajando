ffc756b6423f0ad32d2300cb18b5d840d3b44937
Bririnaa <brisamartino656@gmail.com>
2025-06-29 17:20:12 -0300

    dao paquete

--- src/main/java/com/viajando/dao/paquete/PaqueteDaoImp.java
+++ src/main/java/com/viajando/dao/paquete/PaqueteDaoImp.java
@@ -7,166 +7,196 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.viajando.config.Conexion;
+import com.viajando.dao.excursion.ExcursionDao;
+import com.viajando.dao.excursion.ExcursionDaoImp;
+import com.viajando.dao.hotel.HotelDao;
+import com.viajando.dao.hotel.HotelDaoImp;
+import com.viajando.dao.vuelo.VueloDao;
+import com.viajando.dao.vuelo.VueloDaoImp;
+import com.viajando.domain.Hotel;
+import com.viajando.domain.Excursion;
+import com.viajando.domain.Vuelo;
 import com.viajando.domain.Paquete;
 import com.viajando.exception.ErrorException;
 
 public class PaqueteDaoImp implements PaqueteDao {
 
-    private Conexion conexion = Conexion.getInstance();
+	private Conexion conexion = Conexion.getInstance();
+	private ExcursionDao excursionDao = new ExcursionDaoImp();
+	private VueloDao vueloDao = new VueloDaoImp();
+	private HotelDao hotelDao = new HotelDaoImp();
 
-    private static final String queryConsultarPaquete = "SELECT id, nombre, descripcion, hotel_id, vuelo_id, excursion_id, estrellas, personas, precio FROM paquete WHERE id=?";
-    
-    private static final String queryAddPaquete = "INSERT INTO paquete (nombre, descripcion, hotel_id, vuelo_id, excursion_id, estrellas, personas, precio) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-    
-    private static final String queryUpdateImage = "UPDATE paquete SET imagen=? WHERE id=?";
-    
-    private static final String queryDeletePaquete = "DELETE FROM paquete WHERE id=?";
-    
-    private static final String queryList = "SELECT id, nombre, descripcion, hotel_id, vuelo_id, excursion_id, estrellas, personas, precio FROM paquete";
+	private static final String queryConsultarPaquete = "SELECT id, nombre, descripcion, hotel_id, vuelo_id, excursion_id, estrellas, personas, precio FROM paquete WHERE id=?";
 
-    @Override
-    public List<Paquete> list() throws Exception {
-        ResultSet rs = null;
-        PreparedStatement st = null;
-        List<Paquete> paquetes = new ArrayList<>();
+	private static final String queryAddPaquete = "INSERT INTO paquete (nombre, descripcion, hotel_id, vuelo_id, excursion_id, estrellas, personas, precio) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
-        try {
-            st = conexion.dameConnection().prepareStatement(queryList);
-            rs = st.executeQuery();
+	private static final String queryUpdateImage = "UPDATE paquete SET imagen=? WHERE id=?";
 
-            while (rs.next()) {
-                paquetes.add(new Paquete(
-                    rs.getInt("id"),
-                    rs.getString("nombre"),
-                    rs.getString("descripcion"),
-                    rs.getInt("hotel_id"),
-                    rs.getInt("vuelo_id"),
-                    rs.getInt("excursion_id"),
-                    rs.getDouble("estrellas"),
-                    rs.getInt("personas"),
-                    rs.getInt("precio")
-                ));
-            }
+	private static final String queryDeletePaquete = "DELETE FROM paquete WHERE id=?";
 
-        } finally {
-            if (st != null) st.close();
-            if (rs != null) rs.close();
-        }
+	private static final String queryList = "SELECT id, nombre, descripcion, hotel_id, vuelo_id, excursion_id, estrellas, personas, precio FROM paquete";
 
-        return paquetes;
-    }
+	@Override
+	public List<Paquete> list() throws Exception {
+		ResultSet rs = null;
+		PreparedStatement st = null;
+		List<Paquete> paquetes = new ArrayList<>();
 
-    @Override
-    public Paquete findById(int id) throws Exception {
-        ResultSet rs = null;
-        PreparedStatement st = null;
+		try {
+			st = conexion.dameConnection().prepareStatement(queryList);
+			rs = st.executeQuery();
 
-        try {
-            st = conexion.dameConnection().prepareStatement(queryConsultarPaquete);
-            st.setInt(1, id);
-            rs = st.executeQuery();
+			while (rs.next()) {
+				int hotelId = rs.getInt("hotel_id");
+				Hotel hotel = hotelDao.findById(hotelId);
 
-            if (rs.next()) {
-                return new Paquete(
-                    rs.getInt("id"),
-                    rs.getString("nombre"),
-                    rs.getString("descripcion"),
-                    rs.getInt("hotel_id"),
-                    rs.getInt("vuelo_id"),
-                    rs.getInt("excursion_id"),
-                    rs.getDouble("estrellas"),
-                    rs.getInt("personas"),
-                    rs.getInt("precio")
-                );
-            }
+				int vueloId = rs.getInt("vuelo_id");
+				Vuelo vuelo = vueloDao.findById(vueloId);
 
-        } catch (Exception e) {
-            throw new ErrorException("Hubo un error al realizar la consulta", e);
-        } finally {
-            if (st != null) st.close();
-            if (rs != null) rs.close();
-        }
+				int excursionId = rs.getInt("excursion_id");
+				Excursion excursion = excursionDao.findById(excursionId);
 
-        return null;
-    }
+				paquetes.add(new Paquete(rs.getInt("id"), rs.getString("nombre"), rs.getString("descripcion"), hotel,
+						vuelo, excursion, rs.getDouble("estrellas"), rs.getInt("personas"), rs.getInt("precio")));
+			}
 
-    @Override
-    public int saveAndReturnId(String nombre, String descripcion,
-                               int hotel_id, int vuelo_id, int excursion_id,
-                               double estrellas, int personas, int precio) throws Exception {
-        PreparedStatement st = null;
-        ResultSet rs = null;
-        int idGenerado = -1;
+		} finally {
+			if (st != null)
+				st.close();
+			if (rs != null)
+				rs.close();
+		}
 
-        try {
-            st = conexion.dameConnection().prepareStatement(queryAddPaquete, Statement.RETURN_GENERATED_KEYS);
-            st.setString(1, nombre);
-            st.setString(2, descripcion);
-            
-            if (hotel_id == 0) {
-                st.setNull(3, java.sql.Types.INTEGER);
-            } else {
-                st.setInt(3, hotel_id);
-            }
+		return paquetes;
+	}
 
-            if (vuelo_id == 0) {
-                st.setNull(4, java.sql.Types.INTEGER);
-            } else {
-                st.setInt(4, vuelo_id);
-            }
+	@Override
+	public Paquete findById(int id) throws Exception {
+		ResultSet rs = null;
+		PreparedStatement st = null;
 
-            if (excursion_id == 0) {
-                st.setNull(5, java.sql.Types.INTEGER);
-            } else {
-                st.setInt(5, excursion_id);
-            }
-            
-            st.setDouble(6, estrellas);
-            st.setInt(7, personas);
-            st.setInt(8, precio);
+		try {
+			st = conexion.dameConnection().prepareStatement(queryConsultarPaquete);
+			st.setInt(1, id);
+			rs = st.executeQuery();
 
-            st.executeUpdate();
+			while (rs.next()) {
+	            // HOTEL
+	            int hotelId = rs.getInt("hotel_id");
+	            Hotel hotel = null;
+	            if (!rs.wasNull()) {
+	                hotel = hotelDao.findById(hotelId);
+	            }
 
-            rs = st.getGeneratedKeys();
-            if (rs.next()) {
-                idGenerado = rs.getInt(1);
-            }
-        } finally {
-        	if (rs != null) rs.close();
-            if (st != null) st.close();
-        }
+	            // VUELO
+	            int vueloId = rs.getInt("vuelo_id");
+	            Vuelo vuelo = null;
+	            if (!rs.wasNull()) {
+	                vuelo = vueloDao.findById(vueloId);
+	            }
 
-        return idGenerado;
-    }
+	            // EXCURSION
+	            int excursionId = rs.getInt("excursion_id");
+	            Excursion excursion = null;
+	            if (!rs.wasNull()) {
+	                excursion = excursionDao.findById(excursionId);
+	            }
+				return new Paquete(rs.getInt("id"), rs.getString("nombre"), rs.getString("descripcion"), hotel, vuelo,
+						excursion, rs.getDouble("estrellas"), rs.getInt("personas"), rs.getInt("precio"));
+			}
 
-    @Override
-    public void updateImage(int id, String nombreImagen) throws Exception {
-        PreparedStatement st = null;
-        try {
-            st = conexion.dameConnection().prepareStatement(queryUpdateImage);
-            st.setString(1, nombreImagen);
-            st.setInt(2, id);
-            st.executeUpdate();
-        } finally {
-            if (st != null) st.close();
-        }
-    }
+		} catch (Exception e) {
+			throw new ErrorException("Hubo un error al realizar la consulta", e);
+		} finally {
+			if (st != null)
+				st.close();
+			if (rs != null)
+				rs.close();
+		}
 
-    @Override
-    public void delete(int id) throws Exception {
-        PreparedStatement st = null;
-        try {
-            st = conexion.dameConnection().prepareStatement(queryDeletePaquete);
-            st.setInt(1, id);
-            int rowsAffected = st.executeUpdate();
+		return null;
+	}
 
-            if (rowsAffected == 0) {
-                throw new Error("No se encontró el registro");
-            }
-        } catch (Exception e) {
-            throw new ErrorException("Hubo un error al realizar la consulta", e);
-        } finally {
-            if (st != null) st.close();
-        }
-    }
+	@Override
+	public int saveAndReturnId(String nombre, String descripcion, int hotel_id, int vuelo_id, int excursion_id,
+			double estrellas, int personas, int precio) throws Exception {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		int idGenerado = -1;
+
+		try {
+			st = conexion.dameConnection().prepareStatement(queryAddPaquete, Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, nombre);
+			st.setString(2, descripcion);
+
+			if (hotel_id == 0) {
+				st.setNull(3, java.sql.Types.INTEGER);
+			} else {
+				st.setInt(3, hotel_id);
+			}
+
+			if (vuelo_id == 0) {
+				st.setNull(4, java.sql.Types.INTEGER);
+			} else {
+				st.setInt(4, vuelo_id);
+			}
+
+			if (excursion_id == 0) {
+				st.setNull(5, java.sql.Types.INTEGER);
+			} else {
+				st.setInt(5, excursion_id);
+			}
+
+			st.setDouble(6, estrellas);
+			st.setInt(7, personas);
+			st.setInt(8, precio);
+
+			st.executeUpdate();
+
+			rs = st.getGeneratedKeys();
+			if (rs.next()) {
+				idGenerado = rs.getInt(1);
+			}
+		} finally {
+			if (rs != null)
+				rs.close();
+			if (st != null)
+				st.close();
+		}
+
+		return idGenerado;
+	}
+
+	@Override
+	public void updateImage(int id, String nombreImagen) throws Exception {
+		PreparedStatement st = null;
+		try {
+			st = conexion.dameConnection().prepareStatement(queryUpdateImage);
+			st.setString(1, nombreImagen);
+			st.setInt(2, id);
+			st.executeUpdate();
+		} finally {
+			if (st != null)
+				st.close();
+		}
+	}
+
+	@Override
+	public void delete(int id) throws Exception {
+		PreparedStatement st = null;
+		try {
+			st = conexion.dameConnection().prepareStatement(queryDeletePaquete);
+			st.setInt(1, id);
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected == 0) {
+				throw new Error("No se encontró el registro");
+			}
+		} catch (Exception e) {
+			throw new ErrorException("Hubo un error al realizar la consulta", e);
+		} finally {
+			if (st != null)
+				st.close();
+		}
+	}
 }
